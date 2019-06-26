6b64f1a092c9f33f62c3275d0695a78d68b5fd5c
prajjal saxena <52135244+prajjal1997@users.noreply.github.com>
2019-06-26 11:36:21 -0700

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -3,11 +3,11 @@ public class Pattern1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i,j;                                                //            *
-		for(i=1;i<=5;i++)                                      //            **
-		{                                                      //           ***
-			for(j=1;j<=5;j++)                                  //          ****
-			{                                                  //         *****
+		int i,j;                                                //              *
+		for(i=1;i<=5;i++)                                      //              **
+		{                                                      //             ***
+			for(j=1;j<=5;j++)                                  //        ****
+			{                                                  //       *****
 				if(j>=6-i) 
 					System.out.print("*");
 				else 
